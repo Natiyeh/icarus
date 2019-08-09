0891273f9f34677df510e63a7a5bb14dc8fe4254
Nasser Atiyeh <nasseratiyeh@gmail.com>
2019-08-09 17:40:11 -0500

    did all i could do today

---
+++ app/controllers/pokemon_controller.rb
@@ -0,0 +1,21 @@
+class PokemonController < ApplicationController
+
+  POKEMON = {
+    1 => { name: "Gloom", location: "Route 28", category: "Grass" },
+    2 => { name: "Pikachu", location: "Route 3", category: "Electric" }
+  }
+
+  def index
+    @category = params[:pokemon_type]
+    if params[:pokemon_type].blank?
+      @pokemon = POKEMON
+    else
+      @pokemon = POKEMON.select { |id, r| r[:category] == @category }
+    end
+  end
+
+  def create
+    puts "TODO: create the pokemon in the DB"
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'contact', to: 'pages#contact'
+  get 'about', to: 'pages#about'
+
+  get 'pokemon', to: 'pokemon#index'
+
+  post 'pokemon', to: 'pokemon#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'pages#home', as: :home
 end
