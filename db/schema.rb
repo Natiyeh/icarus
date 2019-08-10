d48d2ee910c9666076749c3644601fff7ffc06c2
Nasser Atiyeh <nasseratiyeh@gmail.com>
2019-08-10 11:28:45 -0500

    added type to pokemons table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_10_161018) do
+ActiveRecord::Schema.define(version: 2019_08_10_162411) do
 
   create_table "pokemons", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2019_08_10_161018) do
     t.string "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "type"
   end
 
 end
