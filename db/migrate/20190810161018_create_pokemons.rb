4d64c44ed42d795fdfd794b01d837225a8a6ab0e
Nasser Atiyeh <nasseratiyeh@gmail.com>
2019-08-10 11:17:02 -0500

    added pokemon model

---
+++ db/migrate/20190810161018_create_pokemons.rb
@@ -0,0 +1,11 @@
+class CreatePokemons < ActiveRecord::Migration[5.2]
+  def change
+    create_table :pokemons do |t|
+      t.string :name
+      t.string :location
+      t.string :category
+
+      t.timestamps
+    end
+  end
+end
